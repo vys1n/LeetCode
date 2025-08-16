d4f7c79c9a5b134102988d3c5b6f14142ec399db
vys1n <kavysingh04@gmail.com>
2025-08-16 11:54:26 +0530

    Two Sum

---
+++ 1.cpp
@@ -0,0 +1,39 @@
+// Two Sum
+
+#include <iostream>
+#include <unordered_map>
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        for (int i = 0; i < nums.size(); i++) {
+            for (int j = 0; j < nums.size(); j++) {
+                if (i != j && i < j) {
+                    if (nums[i] + nums[j] == target) {
+                        return {i,j};
+                    }
+                }
+            }
+        }
+        return {};
+    }
+};
+
+
+// TC - O(n)
+// SC - O(n)
+/* vector<int> twoSum(vector<int>& nums, int target){
+    int n = nums.size();
+    unordered_map<int, int> prevMap;
+
+    for (int i=0; i<n; i++){
+        int diff = target - nums[i];
+        if (prevMap.find(diff) != prevMap.end())
+            return {prevMap[diff], i};
+        prevMap.insert({nums[i], i});
+    }
+    return {};
+} */
+
