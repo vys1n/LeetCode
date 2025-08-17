2d13e935a2585eec8a387a114275059d90f5246a
vys1n <kavysingh04@gmail.com>
2025-08-17 13:47:16 +0530

    Valid Anagram

--- 242.cpp
+++ 242.cpp
@@ -1,3 +1,4 @@
+#include <vector>
 #include <string>
 #include <unordered_map>
 using namespace std;
@@ -16,3 +17,24 @@ public:
         return countS == countT;
     }
 };
+
+// TC - O(n+m)
+// SC - O(1)
+/* bool isAnagram(string s, string t) {
+    if (s.length() != t.length()) {
+        return false;
+    }
+
+    vector<int> count(26, 0);
+    for (int i = 0; i < s.length(); i++) {
+        count[s[i] - 'a']++;
+        count[t[i] - 'a']--;
+    }
+
+    for (int val : count) {
+        if (val != 0) {
+            return false;
+        }
+    }
+    return true;
+} */
