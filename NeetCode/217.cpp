49d7b5b2038d5238c4a17b75784e30484275c0a7
vys1n <kavysingh04@gmail.com>
2025-08-19 17:18:30 +0530

    Encode and Decode Strings

---
+++ NeetCode/217.cpp
@@ -0,0 +1,31 @@
+#include <string>
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+
+    string encode(vector<string>& strs) {
+        string full;
+        for (auto& word : strs) {
+            full = full + to_string(word.size()) + '#' + word;
+        }
+        return full;
+    }
+
+    vector<string> decode(string s) {
+        vector<string> res;
+        int i = 0;
+        while (i < s.size()) {
+            int j = i;
+            while (s[j] != '#') {
+                j++;
+            }
+            int len = stoi(s.substr(i,j-i));
+            string word = s.substr(j + 1, len);
+            res.push_back(word);
+            i = j + 1 + len;
+        }
+        return res;
+    }
+};
