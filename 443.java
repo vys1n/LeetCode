7668df6ff12e3f388dd7be5d7472ed593a2282fb
vys1n <kavysingh04@gmail.com>
2025-10-16 02:33:17 +0530

    String Compression

--- 443.java
+++ 443.java
@@ -1,5 +1,3 @@
-// String Compression
-
 class Solution {
     public int compress(char[] chars) {
         StringBuilder s = new StringBuilder();
@@ -19,3 +17,56 @@ class Solution {
         return s.length();
     }
 }
+
+/*
+public int compress(char[] chars) {
+    int len = chars.length;
+    int i = 0, k = 0;   // i is read pointer, k is write pointer
+    
+    while (i < len) {
+        chars[k++] = chars[i];
+        int j = i + 1;
+        
+        while (j < len && chars[i] == chars[j])
+            j++;
+
+        if (j-i > 1) {
+            String current = String.valueOf(j-i);
+            for (char c : current.toCharArray())
+                chars[k++] = c;
+        }
+
+        i = j;
+    }
+
+    return k;
+}
+
+TC - O(n)
+SC - O(1)
+
+*/
+
+/*
+        int j = 0;
+        int i = 0;
+        int len = chars.length;
+        while(i<len){
+            char first_ch = chars[i];
+            int count = 0;
+            while(i<len && chars[i] == first_ch){
+                count++;
+                i++;
+            }
+            chars[j] = first_ch;
+            j++;
+            if(count>1){
+                String temp = Integer.toString(count);
+                for(char c : temp.toCharArray()){
+                    chars[j] = c;
+                    j++;
+                }
+            }
+        }
+        return j;
+*/
