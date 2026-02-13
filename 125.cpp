291fcc6d82da2c7a4a0e9cbdb9b1bbbb55d1ac9a
vys1n <kavysingh04@gmail.com>
2026-02-13 16:13:23 +0530

    Valid Palindrome

--- 125.cpp
+++ 125.cpp
@@ -1,29 +1,22 @@
 #include <string>
+#include <cctype>
 using namespace std;
 
-class Solution {
-public:
-    bool isPalindrome(string s) {
-        if (s.length() == 1) {
-            return true;
-        }
+bool isPalindrome(string s) {
+    if (s.length() == 1)
+        return true;
 
+    int l = 0, r = s.length() - 1;
+    while(l < r) {
+        if (!isalnum(s[l])) l++;
+        else if (!isalnum(s[r])) r--;
         else {
-            for (int i=0; i<s.length(); i++) {
-                if ((s[i] >= '0' && s[i] <= '9') ||
-                    (s[i] >= 'A' && s[i] <= 'Z') ||
-                    (s[i] >= 'a' && s[i] <= 'z')) {
-                    if (s[i] >= 65 && s[i] <= 90) {
-                        s[i] += 32;
-                    }
-                }
-
-                else {
-                    s.erase(i,1);
-                    i--;
-                }
-            }
+            if (tolower(s[l]) != tolower(s[r]))
+                return false;
+            l++; r--;
         }
-        return false;
     }
-};
+    return true;
+}
+
+
