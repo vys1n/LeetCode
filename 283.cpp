671be62b7f4718de9c44013f786d4f87b7a81ae6
vys1n <kavysingh04@gmail.com>
2026-02-14 20:05:58 +0530

    Move Zeroes

---
+++ 283.cpp
@@ -0,0 +1,14 @@
+#include <utility>
+#include <vector>
+using namespace std; 
+
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+        for (int l=0, r=0; r < nums.size(); r++) {
+            if (nums[r] != 0) {
+                swap(nums[l++], nums[r]);
+            }
+        }
+    }
+};
