e14246f457a18fa65fd1bae70660248179b9a28e
vys1n <kavysingh04@gmail.com>
2026-03-31 15:59:51 +0530

    update: Valid Anagram

--- 242.cpp
+++ 242.cpp
@@ -22,20 +22,21 @@ public:
 // SC - O(1)
 
 /* bool isAnagram(string s, string t) {
-    if (s.length() != t.length()) {
-        return false;
-    }
+    if (s.length() != t.length()) return false;
 
     vector<int> count(26, 0);
-    for (int i = 0; i < s.length(); i++) {
-        count[s[i] - 'a']++;
-        count[t[i] - 'a']--;
+
+    for (char c : s) {
+        count[c - 'a']++;
+    }
+
+    for (char c : t) {
+        count[c - 'a']--;
     }
 
     for (int val : count) {
-        if (val != 0) {
-            return false;
-        }
+        if (val != 0) return false;
     }
+
     return true;
 } */
