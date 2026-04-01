f6bbcb78626e1b210ac6b671a3e69542de33ae44
vys1n <kavysingh04@gmail.com>
2026-04-02 00:47:00 +0530

    First Unique Character in a String

---
+++ 387.cpp
@@ -0,0 +1,18 @@
+#include <string>
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    int firstUniqChar(string s) {
+        vector<int> map(26,0);
+        
+        for (char ch : s) map[ch - 'a'] += 1;
+
+        for (int i=0; i<s.size(); i++) {
+            if (map[s[i] - 'a'] == 1) return i;
+        }
+
+        return -1;
+    }
+};
