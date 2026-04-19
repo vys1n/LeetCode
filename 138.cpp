755fbbbdcd6fb1418c01cd4da341c93a59671dc3
vys1n <kavysingh04@gmail.com>
2026-04-19 18:51:39 +0530

    Copy List with Random Pointer

---
+++ 138.cpp
@@ -0,0 +1,37 @@
+#include <unordered_map>
+using namespace std;
+
+class Node {
+public:
+    int val;
+    Node* next;
+    Node* random;
+    
+    Node(int _val) {
+        val = _val;
+        next = nullptr;
+        random = nullptr;
+    }
+};
+
+class Solution {
+public:
+    Node* copyRandomList(Node* head) {
+        unordered_map<Node*, Node*> map;
+
+        Node* curr = head;
+        while (curr) {
+            map[curr] = new Node(curr->val);
+            curr = curr->next;
+        }
+
+        curr = head;
+        while (curr) {
+            map[curr]->next = map[curr->next];
+            map[curr]->random = map[curr->random];
+            curr = curr->next;
+        }
+
+        return map[head];
+    }
+};
