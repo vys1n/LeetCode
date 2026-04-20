7e54bac41f60acf8ef4f914819adae87dc3cf573
vys1n <kavysingh04@gmail.com>
2026-04-20 15:43:59 +0530

    Implement Queue using Stacks

--- 232.cpp
+++ 232.cpp
@@ -38,12 +38,3 @@ public:
         return s1.empty() && s2.empty();
     }
 };
-
-/**
- * Your MyQueue object will be instantiated and called as such:
- * MyQueue* obj = new MyQueue();
- * obj->push(x);
- * int param_2 = obj->pop();
- * int param_3 = obj->peek();
- * bool param_4 = obj->empty();
- */
