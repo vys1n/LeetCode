44edaff76a71ae30f90d337840f0f151bda8949f
vys1n <kavysingh04@gmail.com>
2026-04-21 23:42:26 +0530

    Online Stock Span

---
+++ 901.cpp
@@ -0,0 +1,20 @@
+#include <stack>
+using namespace std;
+
+class StockSpanner {
+    stack<pair<int,int>> st;
+public:
+    StockSpanner() {}
+    
+    int next(int price) {
+        int span = 1;
+        while (!st.empty() && st.top().first <= price) {
+            span += st.top().second;
+            st.pop();
+        }
+
+        st.push({price, span});
+
+        return span;
+    }
+};
