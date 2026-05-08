07cdf9a9c68dffeff3402a81d62abb8e3c9c0e47
vys1n <kavysingh04@gmail.com>
2026-05-08 11:48:36 +0530

    Evaluate Reverse Polish Notation

---
+++ 150.cpp
@@ -0,0 +1,42 @@
+#include <stack>
+#include <vector>
+#include <string>
+using namespace std;
+
+class Solution {
+public:
+    int evalRPN(vector<string>& tokens) {
+        stack<int> st;
+
+        for (string& c : tokens) {
+            if (c == "+") {
+                int a = st.top(); st.pop();
+                int b = st.top(); st.pop();
+                st.push(b + a);
+            }
+            
+            else if (c == "-") {
+                int a = st.top(); st.pop();
+                int b = st.top(); st.pop();
+                st.push(b - a);
+            }
+            
+            else if (c == "*") {
+                int a = st.top(); st.pop();
+                int b = st.top(); st.pop();
+                st.push(b * a);
+            }
+            
+            else if (c == "/") {
+                int a = st.top(); st.pop();
+                int b = st.top(); st.pop();
+                st.push(b / a);
+            }
+
+            else
+                st.push(stoi(c));
+        }
+
+        return st.top();
+    }
+};
