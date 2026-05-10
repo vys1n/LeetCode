b939541f077a15f333434b8e4e566fc2c58aa2ed
vys1n <kavysingh04@gmail.com>
2026-05-11 02:50:26 +0530

    Binary Tree Preorder Traversal

---
+++ 144.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+#include <vector>
+using namespace std;
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Solution {
+    vector<int> res;
+private:
+    void preorder(TreeNode* node) {
+        if (!node) return;
+
+        res.push_back(node->val);
+        preorder(node->left);
+        preorder(node->right);
+    }
+
+public:
+    vector<int> preorderTraversal(TreeNode* root) {
+        preorder(root);
+        return res;
+    }
+};
