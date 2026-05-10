0738b9678b7ff6aa6e4c7ce2eb150d29a151a665
vys1n <kavysingh04@gmail.com>
2026-05-10 15:33:17 +0530

    Jump Game II

---
+++ 45.cpp
@@ -0,0 +1,22 @@
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    int jump(vector<int>& nums) {
+        int res = 0, l = 0, r = 0;
+
+        while (r < nums.size() - 1) {
+            int far = 0;
+            
+            for (int i = l; i <= r; i++)
+                far = max(far, i + nums[i]);
+
+            l = r + 1;
+            r = far;
+            res++;
+        }
+
+        return res;
+    }
+};
