38d1d1e3f6fbacc3f2359d26956a9ffdd8617647
vys1n <kavysingh04@gmail.com>
2026-05-12 00:15:37 +0530

    Delete Node in a BST

---
+++ 450.cpp
@@ -0,0 +1,57 @@
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Solution {
+public:
+    TreeNode* deleteNode(TreeNode* root, int key) {
+        if (!root) return nullptr;
+
+        if (root->val == key)
+            return helper(root);
+
+        TreeNode* dummy = root;
+        while (root) {
+            if (root->val > key) {
+                if (root->left && root->left->val == key) {
+                    root->left = helper(root->left);
+                    break;
+                } else {
+                    root = root->left;
+                }
+            } else {
+                if (root->right && root->right->val == key) {
+                    root->right = helper(root->right);
+                    break;
+                } else {
+                    root = root->right;
+                }
+            }
+        }
+
+        return dummy;
+    }
+
+    TreeNode* helper(TreeNode* root) {
+        if (root->left == nullptr)
+            return root->right;
+        if (root->right == nullptr)
+            return root->left;
+
+        TreeNode* rc = root->right;
+        TreeNode* lastRight = findLastRight(root->left);
+        lastRight->right = rc;
+
+        return root->left;
+    }
+
+    TreeNode* findLastRight(TreeNode* root) {
+        if (!root->right) return root;
+        return findLastRight(root->right);
+    }
+};
