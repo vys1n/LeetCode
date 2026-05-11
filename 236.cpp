1368530e68c4a14952a4a0bbda6b6765bb79c671
vys1n <kavysingh04@gmail.com>
2026-05-11 14:48:10 +0530

    Lowest Common Ancestor of Binary Tree

---
+++ 236.cpp
@@ -0,0 +1,20 @@
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+};
+
+class Solution {
+public:
+    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+        if (root == nullptr || root == p || root == q) return root;
+
+        TreeNode* left = lowestCommonAncestor(root->left, p, q);
+        TreeNode* right = lowestCommonAncestor(root->right, p, q);
+
+        if (left == nullptr) return right;
+        else if (right == nullptr) return left;
+        else return root;
+    }
+};
